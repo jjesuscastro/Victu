aa1c33231702797f757b3b53c2ccfa2950221d26
Jesus Castro <jjoesei.castro@gmail.com>
2023-07-08 18:46:58 +0800

    add expand placeholder

--- lib/screens/menu_page.dart
+++ lib/screens/menu_page.dart
@@ -63,72 +63,97 @@ class MenuPage extends StatelessWidget {
 }
 
 Widget dayCard(String day, String date) {
-  return Card(
-    margin: const EdgeInsets.fromLTRB(0, 0, 0, 16),
-    color: const Color(0xffffffff),
-    shadowColor: const Color(0x4d939393),
-    elevation: 1,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(4.0),
-      side: const BorderSide(color: Color(0x4d9e9e9e), width: 1),
-    ),
-    child: ExpandablePanel(
-      header: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            width: 65,
-            alignment: Alignment.center,
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.all(16),
-            decoration: const BoxDecoration(
-              color: Color(0xfff2f2f2),
-              shape: BoxShape.circle,
-            ),
-            child: Text(
-              day,
-              textAlign: TextAlign.start,
-              overflow: TextOverflow.clip,
-              style: const TextStyle(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.normal,
-                fontSize: 14,
-                color: Color(0xff000000),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
-                    date,
-                    textAlign: TextAlign.start,
-                    maxLines: 1,
-                    overflow: TextOverflow.clip,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.normal,
-                      fontSize: 16,
-                      color: Color(0xff000000),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+  return ExpandableNotifier(
+    child: Card(
+      margin: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+      color: const Color(0xffffffff),
+      shadowColor: const Color(0x4d939393),
+      elevation: 1,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        side: const BorderSide(color: Color(0x4d9e9e9e), width: 1),
       ),
-      collapsed: Container(height: 10),
-      expanded: const Text(
-          "expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded expanded "),
+      child: ScrollOnExpand(
+        child: ExpandablePanel(
+          header: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: 65,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  color: Color(0xfff2f2f2),
+                  shape: BoxShape.circle,
+                ),
+                child: Text(
+                  day,
+                  textAlign: TextAlign.start,
+                  overflow: TextOverflow.clip,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 14,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        date,
+                        textAlign: TextAlign.start,
+                        maxLines: 1,
+                        overflow: TextOverflow.clip,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 16,
+                          color: Color(0xff000000),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          collapsed: Container(height: 10),
+          expanded: const Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Column(children: [
+                  Text("Breakfast"),
+                  Text("Longganisa"),
+                  Text("Bacon"),
+                ]),
+                Column(children: [
+                  Text("Lunch"),
+                  Text("Longganisa"),
+                  Text("Bacon"),
+                ]),
+                Column(children: [
+                  Text("Dinner"),
+                  Text("Longganisa"),
+                  Text("Bacon"),
+                ]),
+              ],
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
